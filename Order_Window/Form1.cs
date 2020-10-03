ed2f786e3e56d287d1b7f8b399ecd6c7256d3749
AllenW1998 <50045431+AllenW1998@users.noreply.github.com>
2020-10-03 22:39:57 +0800

    Add files via upload

---
+++ Order_Window/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Order_Window
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //订单展示用list,可以参考课程代码中那个左右传东西的
+        //addorder时新弹出一个窗口，新弹出的窗口参考课程代码中的groupbox/register
+        //如何打开新窗口，可以参考multiform
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
