60925a60d2ac04e2f72b6d6301141ab734f547db
AllenW1998 <50045431+AllenW1998@users.noreply.github.com>
2020-10-05 02:32:10 +0800

    仍需进一步完善

---
+++ Order_Window/Order.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order_Window
+{
+    [Serializable]
+    public class Order
+    {
+        public long ID { get; set; }
+        public string GoodsName { get; set; }
+        public string Customer { get; set; }
+        public decimal Price { get; set; }
+
+        public Order(long iD, string goodsName, string customer, decimal price)
+        {
+            ID = iD;
+            GoodsName = goodsName ?? throw new ArgumentNullException(nameof(goodsName));
+            Customer = customer ?? throw new ArgumentNullException(nameof(customer));
+            this.Price = price;
+        }
+
+        public Order()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            string str = "该订单信息如下： ID:" + this.ID.ToString() + " GoodsName:" + this.GoodsName + " Customer:" + this.Customer + " Price:" + this.Price.ToString();
+            return str;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Order)) return false;
+            return this.ID == ((Order)obj).ID;
+        }
+        //Equals不好记，顺便把操作符也重载了
+        public static bool operator ==(Order lhs, Order rhs)
+        {
+            return (lhs.ID == rhs.ID);
+        }
+        public static bool operator !=(Order lhs, Order rhs)
+        {
+            return (lhs.ID != rhs.ID);
+        }
+    }
+}
