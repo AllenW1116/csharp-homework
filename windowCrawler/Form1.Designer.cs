a07c06688875f5949f623d31a3ca2757bc294441
AllenW <50045431+AllenW1116@users.noreply.github.com>
2020-10-12 01:27:00 +0800

    添加窗口

---
+++ windowCrawler/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace windowCrawler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView = new System.Windows.Forms.ListView();
+            this.urltextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(12, 107);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(776, 331);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // urltextBox
+            // 
+            this.urltextBox.Font = new System.Drawing.Font("宋体", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.urltextBox.Location = new System.Drawing.Point(126, 36);
+            this.urltextBox.Name = "urltextBox";
+            this.urltextBox.Size = new System.Drawing.Size(486, 27);
+            this.urltextBox.TabIndex = 1;
+            this.urltextBox.Text = "https://www.cnblogs.com/";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(28, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "起始网址";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("宋体", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(641, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 27);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "开始爬取";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "详细信息";
+            this.columnHeader1.Width = 1020;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.urltextBox);
+            this.Controls.Add(this.listView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.TextBox urltextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ windowCrawler/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windowCrawler
+{
+    public partial class Form1 : Form
+    {
+        public string Download(string url)  //将url指向的html文件之源码下载下来，返回string
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;   //webclient是c#的NET包中自带的工具
+                string html = webClient.DownloadString(url);//将url指向的html文件之源码下载并存入html
+
+                //Console.WriteLine(html);
+
+                //string fileName = count.ToString();
+                string fileName = @"‪Test" + count.ToString() + ".txt";
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                listView.Items.Add(new ListViewItem("下载错误：" + ex.Message));
+
+                 
+                return "";
+
+            }
+        }
+        public void Parse(string html, string root)
+        {
+
+            //在下载好的网页源码中，找href标签。
+            string strRef = @"(href|HREF)[ ]*=[ ]*[""'][^""'#>]+(\.)(html|htm|aspx|jsp|php)[""']";//只爬取html，htm，jsp，aspx、php页面
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            //href是网页超链接的一个属性，它后面写一个url
+            //点击这个超链接，就会跳转到该url指向的资源（可能是一个服务器本地文件，也可能是一个新的网页）
+            //如果一个网页源码中有写另一个网址，这个网址一定写在href属性中
+
+
+            foreach (Match match in matches)
+            {
+                //fatherurl是指：
+
+                strRef = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '\"', '#', '>');
+                //match.value是匹配成功的子串，即  href=“字母数字组合” ；从子串里面找到=后面的部分，which is a url
+                //strRef里存的就是我们要下载的地址了。
+                //它可能是绝对地址，形如：https://www.cnblogs.com/binghe001/p/13794228.html
+                //也可能是相对地址，形如：test/index.html 或 ./test/index.html
+                //
+
+                if (strRef.Length == 0) continue;
+                if (!strRef.Contains("https://www."))
+                {
+                    string temp = strRef;
+                    if (temp.Substring(0, 1) == "./") { temp = strRef.Substring(2, strRef.Length); }
+
+                    urls[root + temp] = false;
+
+                }
+                if (urls[strRef] == null && strRef.Contains("/www.cnblogs.")) urls[strRef] = false;//hashtable里没有，那就加入并置为false准备下载！
+            }
+        }
+
+
+        private void Crawl()
+        {
+            
+            while (true)
+            {
+                string current = null;
+                foreach (string url in urls.Keys)
+                {
+                    if ((bool)urls[url]) continue;//hashtable根据key值（string）找value（Boolean）
+                    current = url;
+                }
+                if (current == null | count > 100) break;
+                //Console.WriteLine("============================================================================");
+                //Console.WriteLine("爬行" + current + "页面！");
+
+                //listView.Items.Add(new ListViewItem("爬行" + current + "页面！"));
+                listViewItems.Add(new ListViewItem());//匿名变量添加一个ListViewItem
+                listViewItems.Last().SubItems[0].Text = "爬行" + current + "页面！";
+                listView.Items.Add(listViewItems.Last());
+
+                string html = Download(current);  //将url指向的html文件之源码下载下来，返回string
+
+                urls[current] = true;
+                count++;
+
+                Parse(html, current);
+            }
+            //listView.Items.Add(new ListViewItem("爬行结束！" ));
+            listViewItems.Add(new ListViewItem());//匿名变量添加一个ListViewItem
+            listViewItems.Last().SubItems[0].Text = "爬行结束！";
+            listView.Items.Add(listViewItems.Last());
+
+        }
+
+        private Hashtable urls = new Hashtable();
+        private int count = 0;
+        public List<ListViewItem> listViewItems = new List<ListViewItem>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)//开始爬取
+        {
+            
+
+            string starturl = urltextBox.Text;
+            
+
+            urls.Add(starturl, false); //初始状态为false，等下开始下载;urls是一个HashTable<key:string , value:boolean>
+
+            new Thread(Crawl).Start();
+        }
+    }
+}
